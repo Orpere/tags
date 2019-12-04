44a5a91978e6605a0b3473dfa7685ca4938680e1
orpere <orlando.capoeiraraiz@gmail.com>
2019-12-04 16:43:44 +0100

    null_resource

--- terraform.tf
+++ terraform.tf
@@ -1,26 +1,18 @@
-# The default provider configuration
-provider "aws" {
-  region     = "us-east-1"
-}
-
-# Define needed variables for the credentials
-# export AWS_ACCESS_KEY_ID="anaccesskey"
-# export AWS_SECRET_ACCESS_KEY="asecretkey"
-# export AWS_DEFAULT_REGION="us-west-2"
 variable "default_tags" {
   type = "map"
-
   default = {
-    Name = "value"
+    Name = "XXXXXXXXXXXXXXXXXXXX",
+    owner = "test"
   }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0994c095691a46fb5"
-  instance_type = "t2.micro"
+resource "null_resource" "cluster" {
 
-
-  tags = "${merge(map( 
-            "Name", "value"
+  triggers = "${merge(map( 
+            "Name", "XXXXXXXXXXXXXXXXXXXX"
         ), var.default_tags)}"
+}
+
+output "tags" {
+  value = null_resource.cluster.triggers
 }
